73faf6ecfb8a58262b10e92e1df9d8483aa52297
Pradeep Banavara <pradeep@Pradeeps-MacBook-Air.local>
2014-09-20 11:32:57 +0530

    Initial Commit

---
+++ seqhack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  seqhack
+//
+//  Created by Pradeep Banavara on 20/09/14.
+//  Copyright (c) 2014 Pradeep Banavara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
